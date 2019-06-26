2b11d2c0a0d91680c2aa056d118ed8948b670f9f
Zhangling <a258456z@qq.com>
2019-06-26 09:04:45 +0800

    hello Theshy and Jackeylove

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello Java");
         System.out.println("Hello Theshy");
+        System.out.println("Hello Jackeylove");
     }
 }
